a2986a1562df3a8b1648036abd7768f14d95ac0e
Naimar1 <naiglyme1@gmail.com>
2019-02-04 18:32:38 +0200

    Make an import of injectable

--- src/app/gitform/gitform.component.ts
+++ src/app/gitform/gitform.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Injectable } from '@angular/core';
 @Component({
   selector: 'app-gitform',
   templateUrl: './gitform.component.html',
